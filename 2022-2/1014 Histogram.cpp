c57446231378c7ae7ac9b913c373cce4f083a999
20220086 <kilbddg@icloud.com>
2022-10-19 16:04:08 +0900

    1019 assignment update

--- 2022-2/1014 Histogram.cpp
+++ 2022-2/1014 Histogram.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
+#include <algorithm>
 using namespace std;
 int main()
 {
-	const int CAPACITY = 10;
-	int frequencies[CAPACITY] = { 0 };
+	const int CAPACITY = 30;
+	char frequencie[CAPACITY] = { 0 };
 	ifstream integerFile;
 	integerFile.open("integerFile.dat");
 	if (!integerFile)
@@ -14,13 +15,35 @@ int main()
 		return 0;
 	}
 
-	int data;
+	/*int data;
 	int size = 0;
-	while (integerFile >> data)
+	for (int i = 0; i < 5; i++)
 	{
-		size++;
-		frequencies[data]++;
+		frequencies[i] = ;
 		cout << frequencies[data];
 	}
 	integerFile.close();
+	cout << frequencies;*/
+
+
+	int i = 0;
+	for (int j = 0; j < 10; j++)
+	{
+		integerFile >> frequencie[i];
+		i++;
+	}
+	cout << endl << frequencie << endl;
+	cout << "count 0 : " << count(frequencie, frequencie + 10, 1) << endl;
+	cout << "count 1 : " << count(frequencie, frequencie + 10, 1) << endl;
+	cout << "count 2 : " << count(frequencie, frequencie + 10, 2) << endl;
+	cout << "count 3 : " << count(frequencie, frequencie + 10, 3) << endl;
+	cout << "count 4 : " << count(frequencie, frequencie + 10, 4) << endl;
+	cout << "count 5 : " << count(frequencie, frequencie + 10, 5) << endl;
+	cout << "count 6 : " << count(frequencie, frequencie + 10, 6) << endl;
+	cout << "count 7 : " << count(frequencie, frequencie + 10, 7) << endl;
+	cout << "count 8 : " << count(frequencie, frequencie + 10, 8) << endl;
+	cout << "count 9 : " << count(frequencie, frequencie + 10, 9) << endl;
+	return 0;
 }
+
+
---
+++ 2022-2/1019 sun.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+// rowsize를 사용하게 수정하기
+void rowTransform(const int originArray[][4], int rowSize, int rowArray[])
+{
+	for (int i = 0; i < 4; i++)
+	{
+		rowArray[i] = originArray[0][i];
+	}
+	for (int j = 0; j < 4; j++)
+	{
+		rowArray[j+4] = originArray[1][j];
+	}
+}
+
+void colTransform(int originArray[][4], int rowSize, int colArray[])
+{
+	for (int i = 0; i < 4; i++)
+	{
+		colArray[i*2] = originArray[0][i];
+	}
+	for (int j = 0; j < 4; j++)
+	{
+		colArray[j*2+1] = originArray[1][j];
+	}
+}
+void printTwoDimensional(const int twoDimensional[][4], int rowSize)
+{
+	for (int i = 0; i < rowSize; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			cout << setw(4) << twoDimensional[i][j];
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
+void printOneDimensional(const int oneDimensional[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << setw(4) << oneDimensional[i];
+	}
+	cout << endl;
+}
+int main()
+{
+	int originArray[2][4] = { {0, 1, 2, 3}, {10, 11, 12, 13} };
+	int rowArray[8];
+	int colArray[8];
+
+	rowTransform(originArray, 2, rowArray);
+	colTransform(originArray, 2, colArray);
+
+	cout << " Original Array " << endl;
+	printTwoDimensional(originArray, 2);
+
+	cout << "Row-Transformed Array: ";
+	printOneDimensional(rowArray, 8);
+
+	cout << "Column-Transformed Array: ";
+	printOneDimensional(colArray, 8);
+	return 0;
+}
